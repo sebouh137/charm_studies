7deb8aa4cb431fd9f1841c2c4063a7166090a053
sebouh137 <sebouh.paul@gmail.com>
2022-09-12 23:20:06 -0400

    refactored submit.sh.  env. variables for INPUT_DIR and OUTPUT_DIR

--- submit.sh
+++ submit.sh
@@ -5,18 +5,24 @@
 #SBATCH --cpus-per-task=1
 ####  SBATCH --partition=short
 #SBATCH --time=00:20:00
-#SBATCH --array=0-185
+
+##set this to the number of files in INPUT_DIR
+#SBATCH --array=0-185   
 #SBATCH --mem-per-cpu=16000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=sebouh.paul@gmail.com
 srun hostname
 
 echo $SLURM_ARRAY_TASK_ID
-FILE=`ls /cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v1/dst/train/ElecFTKaon/ElecFTKaon_00*.hipo | awk '{if (NR=='$SLURM_ARRAY_TASK_ID') print $0;}' `
+
+INPUT_DIR=/cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v1/dst/train/ElecFTKaon/
+OUTPUT_DIR=/work/clas12/spaul/charm_hists/
+
+FILE=`ls ${INPUT_DIR}/*.hipo | awk '{if (NR=='$SLURM_ARRAY_TASK_ID') print $0;}' `
 
 
 echo ${FILE}
-OUTFILE=/work/clas12/spaul/charm_hists/$(basename ${FILE} .hipo).root
+OUTFILE=${OUTPUT_DIR}/$(basename ${FILE} .hipo).root
 
 
 srun clas12root -l -b -q /home/spaul/charm_studies/src/MakeHistograms.C+ --in=$FILE --out=$OUTFILE --FTonly
